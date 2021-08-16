7240cedde3eada028d1f943099b4ae0a856a951a
Curtis <lucidck17@gmail.com>
2021-08-16 15:41:45 -0700

    Filter Todos JS

--- ninja-todo-two/app.js
+++ ninja-todo-two/app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
 
 const generateTemplate = (todo) => {
   const html = `<li
@@ -31,4 +32,21 @@ list.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
   }
+});
+
+// Filter todos
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.includes(term))
+    .forEach((todo) => todo.classList.add("filtered"));
+
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
+
+// Keyup Event
+search.addEventListener("keyup", () => {
+  const term = search.value.trim();
+  filterTodos(term);
 });
